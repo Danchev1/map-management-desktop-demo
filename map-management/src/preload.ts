b0a1dbfc08c9dfa4bf3cb1b144f25ad9bcba67d7
yordandanchev <yordangdanchev@gmail.com>
2023-08-07 17:01:22 +0300

    init electron app

--- map-management/src/preload.ts
+++ map-management/src/preload.ts
@@ -1,2 +1,30 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+import {
+    contextBridge,
+    ipcRenderer
+} from 'electron';
+
+// Expose protected methods that allow the renderer process to use
+// the ipcRenderer without exposing the entire object
+contextBridge.exposeInMainWorld(
+    "api", {
+        send: (channel: string, data: any) => {
+            console.log('send', channel, data);
+            // whitelist channels
+            let validChannels = ["toMain"];
+            if (validChannels.includes(channel)) {
+                ipcRenderer.send(channel, data);
+            }
+        },
+        receive: (channel: string, func: (arg0: any) => void) => {
+            console.log('receive', channel);
+            let validChannels = ["fromMain"];
+            if (validChannels.includes(channel)) {
+                // Deliberately strip event as it includes `sender`
+                // @ts-ignore
+                ipcRenderer.on(channel, (event, ...args) => func(...args));
+            }
+        }
+    }
+);
